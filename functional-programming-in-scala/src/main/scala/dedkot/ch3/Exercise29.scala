793dd248894958d6221f7881ced4cc508bf463ef
dedkot <dedkot01@gmail.com>
2021-07-09 11:58:23 +0300

    functional-programming-in-scala: added all methods in ex 3.29

--- functional-programming-in-scala/src/main/scala/dedkot/ch3/Exercise29.scala
+++ functional-programming-in-scala/src/main/scala/dedkot/ch3/Exercise29.scala
@@ -3,3 +3,12 @@ package dedkot.ch3
 def fold[A,B](t: Tree[A])(f: A => B)(b: (B,B) => B): B = t match
   case Leaf(v) => f(v)
   case Branch(l, r) => b(fold(l)(f)(b), fold(r)(f)(b))
+
+def sizeViaFold[A](t: Tree[A]): Int = fold(t)(_ => 1)(1 + _ + _)
+
+def maximumViaFold(t: Tree[Int]): Int = fold(t)(identity)(_ max _)
+
+def depthViaFold[A](t: Tree[A]): Int = fold(t)(v => 1)((l, r) => 1 + (l max r))
+
+def mapViaFold[A, B](t: Tree[A])(f: A => B): Tree[B] = 
+  fold(t)(v => Leaf(f(v)): Tree[B])((l, r) => Branch(l, r))
--- functional-programming-in-scala/src/test/scala/dedkot/ch3/Exercise29Spec.scala
+++ functional-programming-in-scala/src/test/scala/dedkot/ch3/Exercise29Spec.scala
@@ -16,3 +16,48 @@ class Exercise29Spec:
 
     val t2 = Branch(b, b)
     assertEquals(4, fold(t2)(identity)(_ + _))
+
+  @Test def testSize: Unit =
+    val l = Leaf(1)
+    assertEquals(1, sizeViaFold(l))
+
+    val b = Branch(l, l)
+    assertEquals(3, sizeViaFold(b))
+
+    val t = Branch(l, b)
+    assertEquals(5, sizeViaFold(t))
+
+    val t2 = Branch(b, b)
+    assertEquals(7, sizeViaFold(t2))
+
+  @Test def testMaximum: Unit =
+    val l = Leaf(1)
+    assertEquals(1, maximumViaFold(l))
+
+    val l3 = Leaf(3)
+    val b = Branch(l3, l)
+    assertEquals(3, maximumViaFold(b))
+
+    val t = Branch(l, b)
+    assertEquals(3, maximumViaFold(t))
+
+    val t2 = Branch(b, b)
+    assertEquals(3, maximumViaFold(t2))
+    
+  @Test def testDepth: Unit =
+    val l = Leaf(1)
+    assertEquals(1, depthViaFold(l))
+
+    val b = Branch(l, l)
+    assertEquals(2, depthViaFold(b))
+
+    val t = Branch(l, b)
+    assertEquals(3, depthViaFold(t))
+
+    val t2 = Branch(b, b)
+    assertEquals(3, depthViaFold(t2))
+
+  @Test def testMap: Unit =
+    val t = Branch(Leaf(1), Branch(Leaf(2), Leaf(3)))
+    val expectedTree = Branch(Leaf(2), Branch(Leaf(4), Leaf(6)))
+    assertEquals(expectedTree, mapViaFold(t)(_ * 2))
