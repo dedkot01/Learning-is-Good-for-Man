8ee658ebf13ee52dcea6a90b4277693a8aef0c5e
dedkot <dedkot01@gmail.com>
2021-07-20 17:13:01 +0300

    functional-programming-in-scala: complete ex 5.2

--- functional-programming-in-scala/src/main/scala/dedkot/ch5/Stream.scala
+++ functional-programming-in-scala/src/main/scala/dedkot/ch5/Stream.scala
@@ -1,6 +1,8 @@
 package dedkot.ch5
 
-import Stream.*
+import dedkot.ch5.Stream.empty
+
+import Stream._
 
 trait Stream[+A]:
 
@@ -21,9 +23,14 @@ trait Stream[+A]:
     case Empty => None
     case Cons(h, t) => if (f(h())) Some(h()) else t().find(f)
 
-  def take(n: Int): Stream[A] = ???
+  def take(n: Int): Stream[A] = this match
+    case Cons(h, t) if n > 1 => cons(h(), t().take(n - 1))
+    case Cons(h, _) if n == 1 => cons(h(), empty)
+    case _ => empty
 
-  def drop(n: Int): Stream[A] = ???
+  def drop(n: Int): Stream[A] = this match
+    case Cons(_, t) if n > 0 => t().drop(n - 1)
+    case _ => this
 
   def takeWhile(p: A => Boolean): Stream[A] = ???
 
--- functional-programming-in-scala/src/test/scala/dedkot/ch5/StreamSpec.scala
+++ functional-programming-in-scala/src/test/scala/dedkot/ch5/StreamSpec.scala
@@ -4,8 +4,21 @@ import org.junit.Test
 import org.junit.Assert.*
 
 class StreamSpec:
+  val l12 = List(1, 2)
+  val l23 = List(2, 3)
+  val l123 = List(1, 2, 3)
+  val s123 = Stream(1, 2, 3)
+
   @Test def testToList: Unit =
-    val l = List(1, 2, 3)
-    val s = Stream(1, 2, 3)
-    assertEquals(l, s.toList)
+    assertEquals(l123, s123.toList)
     assertEquals(List.empty, Stream().toList)
+
+  @Test def testTake: Unit = {
+    assertEquals(l12, s123.take(2).toList)
+    assertEquals(l123, s123.take(5).toList)
+  }
+
+  @Test def testDrop: Unit = {
+    assertEquals(l23, s123.drop(1).toList)
+    assertEquals(List.empty, s123.drop(3).toList)
+  }
