324dc4268f35c65528e5b7442c3b4e577fd95a5d
dedkot <dedkot01@gmail.com>
2021-12-14 13:07:43 +0300

    pyspark streaming 1

---
+++ python/exercise-with-pyspark/streaming/kafka/kafka.py
@@ -0,0 +1,13 @@
+from pyspark.sql import SparkSession
+
+if __name__ == '__main__':
+    spark = SparkSession.builder.appName("KafkaConsumer").getOrCreate()
+    spark.sparkContext.setLogLevel('ERROR')
+
+    df = spark\
+        .readStream\
+        .format('kafka')\
+        .option('kafka.bootstrap.servers', 'localhost:9092')\
+        .option('subscribe', 'eva00')\
+        .load()
+    print(df.selectExpr('CAST(key AS STRING)', 'CAST(value AS STRING)'))
