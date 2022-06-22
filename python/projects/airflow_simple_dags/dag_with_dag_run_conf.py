3434f00f0289aa35f94469663e9b512eac662429
dedkot01 <dedkot01@gmail.com>
2022-06-22 21:35:46 +0300

    learned dag_run_conf

---
+++ python/projects/airflow_simple_dags/dag_with_dag_run_conf.py
@@ -0,0 +1,32 @@
+from datetime import datetime, timedelta
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator
+
+
+def func(text: str, number: int) -> None:
+    print(text)
+    print(number)
+
+
+def main(text, number, **kwargs):
+    text = kwargs['dag_run'].conf.get('text', text)
+    number = kwargs['dag_run'].conf.get('number', number)
+    func(text, number)
+
+
+with DAG(
+    dag_id='dag_run_conf',
+    schedule_interval=timedelta(seconds=30),
+    start_date=datetime(2021, 12, 30),
+    catchup=False,
+) as dag:
+    func_task=PythonOperator(
+        task_id='func_task',
+        python_callable=main,
+        op_kwargs={
+            'text': 'from_dag',
+            'number': 1,
+        },
+        provide_context=True,
+    )
