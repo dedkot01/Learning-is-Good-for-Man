4a231a23e571710b116f2e6a105386140c494b14
dedkot01 <dedkot01@gmail.com>
2022-08-10 17:08:44 +0300

    init project with ipywidgets

---
+++ python/projects/ipywidgets/MyForm.py
@@ -0,0 +1,17 @@
+from ipywidgets import IntProgress
+from IPython.display import display
+import time
+
+
+class MyForm:
+    def __init__(self) -> None:
+        self.prgBar = IntProgress(min = 0, max = 100) # Создаем прогрессбар
+
+    def show(self):
+        display(self.prgBar) # Выводим прогрессбар на экран
+
+        while self.prgBar.value < self.prgBar.max:   # Пока положение не дошло до максимума - продолжаем цикл
+            self.prgBar.value = self.prgBar.value + 1 # Двигаем "полоску"
+            time.sleep(0.1)
+            
+        print('Процесс завершен')
