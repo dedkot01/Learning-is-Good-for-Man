71e0f56ae56295ea2d1a1ae5f86bca8644f60e4d
dedkot01 <dedkot01@gmail.com>
2022-12-22 17:24:55 +0300

    completed some ex for go

--- golang/hello_world/simple_control.go
+++ golang/hello_world/simple_control.go
@@ -31,6 +31,46 @@ func task2() {
 	}
 }
 
+func task3() {
+	var x int
+	fmt.Scan(&x)
+
+	switch {
+	case x < 10:
+		fmt.Println(x)
+	case x < 100:
+		fmt.Println(x / 10)
+	case x < 1000:
+		fmt.Println(x / 100)
+	case x < 10000:
+		fmt.Println(x / 1000)
+	case x < 100000:
+		fmt.Println(x / 10000)
+	}
+}
+
+func is_lucky_ticket() {
+	var x int
+	fmt.Scan(&x)
+
+	if x % 10 + x % 100 / 10 + x % 1000 / 100 == x % 10000 / 1000 + x % 100000 / 10000 + x % 1000000 / 100000 {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
+
+func is_leap_year() {
+	var x int
+	fmt.Scan(&x)
+
+	if (x % 400 == 0) || (x % 4 == 0 && x % 100 != 0) {
+		fmt.Println("YES")
+	} else {
+		fmt.Println("NO")
+	}
+}
+
 func main() {
-	task2()
+	task3()
 }
